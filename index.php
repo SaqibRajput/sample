4c6db0c48bcdcf349b79e8dec928c2717db65191
Muhammad Saqib. Rajput <saqib.rajput@dc1-arpatechonline.com>
2018-02-15 17:31:29 +0500

    update

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php
 
 
-echo 'First Index Page';
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test GIT</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<span class="heading">Saqib Rajput</span><br/>
+	<span class="mobile-no">03006710419</span><br/>
+</body>
+</html>
